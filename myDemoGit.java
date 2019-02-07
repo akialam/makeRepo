39b032931aa3e5391591de1d5b15ae3a4c8cf973
Aaquib Alam <aki.mohd81@gmail.com>
2019-02-07 16:00:40 +0530

    methodAddes

--- myDemoGit.java
+++ myDemoGit.java
@@ -1,8 +1,16 @@
 class myDemoGit
 {
+  
+  void test()
+  {
+    System.out.println("Aaquib");
+  }
 public static void main(String[] args)
 
 {
+  myDemoGit g=new myDemoGit();
+  g.test();
 System.out.println("welcome in git hub");
+  
 }
 }
